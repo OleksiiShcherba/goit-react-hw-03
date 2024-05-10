9991a58aa1e5998b1b8362afdd1ced3f5679f1b1
Oleksii <shcherbaalex77@gmail.com>
2024-05-10 18:08:03 +0200

    finish home work

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
 import "./App.css";
 import "modern-normalize";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import ContactForm from "./components/ContactForm/ContactForm";
 import SearchBox from "./components/SearchBox/SearchBox";
 import ContactList from "./components/ContactList/ContactList";
 
-const phoneList = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+import phoneList from "./PhoneList.json";
+
+const localStorageKey = "contacts";
 
 function App() {
-  const [contacts, setContacts] = useState(phoneList);
+  const [contacts, setContacts] = useState(() => {
+    const contactFromStorage = window.localStorage.getItem(localStorageKey);
+    if (contactFromStorage) {
+      return JSON.parse(contactFromStorage);
+    } else {
+      return phoneList;
+    }
+  });
   const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    window.localStorage.setItem(localStorageKey, JSON.stringify(contacts));
+  }, [contacts]);
 
   const getFiltratedList = () => {
     const searchableLine = search?.toLowerCase();
@@ -42,12 +50,12 @@ function App() {
   };
 
   return (
-    <>
+    <div className="mainBlock">
       <h1>Phonebook</h1>
       <ContactForm addNewContact={addNewContact} />
       <SearchBox searchField={search} searchChange={setSearch} />
       <ContactList contacts={getFiltratedList()} onDelete={deleteContact} />
-    </>
+    </div>
   );
 }
 
